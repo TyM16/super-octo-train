dec1edbe96ec64db31e3661f8ff3a959ea497f0e
Michael Gamlem III <mgamlem3@gmail.com>
2019-05-01 10:08:15 -0700

    more things I should have saved before committing

--- MP3/FINAL/mips.srcs/sources_1/imports/new/mips_controller.vhd
+++ MP3/FINAL/mips.srcs/sources_1/imports/new/mips_controller.vhd
@@ -14,25 +14,35 @@ end;
 
 architecture struct of controller is
   component maindec
-    port(op:                 in  STD_LOGIC_VECTOR(5 downto 0);
-         memtoreg, memwrite: out STD_LOGIC;
-         branch, alusrc:     out STD_LOGIC;
-         regdst, regwrite:   out STD_LOGIC;
-         jump:               out STD_LOGIC;
-         aluop:              out  STD_LOGIC_VECTOR(1 downto 0));
+    port(op:                               in  STD_LOGIC_VECTOR(5 downto 0);
+         memtoreg, memwrite, memread:      out STD_LOGIC; -- we may not need mem to reg it may be 
+         branch, bne, alusrc:              out STD_LOGIC;
+         regdest:                          out STD_LOGIC_VECTOR(3 downto 0);
+         regwrite:                         out STD_LOGIC;
+         jump:                             out STD_LOGIC;
+         aluop:                            out  STD_LOGIC_VECTOR(3 downto 0));
   end component;
-  component aludec
-    port(funct:      in  STD_LOGIC_VECTOR(5 downto 0);
-         aluop:      in  STD_LOGIC_VECTOR(1 downto 0);
-         alucontrol: out STD_LOGIC_VECTOR(2 downto 0));
-  end component;
+  component ALU
+    generic(width: integer := 16);
+     port(input_a, input_b:  in STD_LOGIC_VECTOR((width-1) downto 0);
+         shift:              in STD_LOGIC_VECTOR(3 downto 0);
+         alucontrol:         in STD_LOGIC_VECTOR(3 downto 0);
+         result:             inout STD_LOGIC_VECTOR((width-1) downto 0);
+         zero:               out STD_LOGIC);
+   end component;
+--  component aludec
+--    port(funct:      in  STD_LOGIC_VECTOR(5 downto 0);
+--         aluop:      in  STD_LOGIC_VECTOR(1 downto 0);
+--         alucontrol: out STD_LOGIC_VECTOR(2 downto 0));
+--  end component;
+
   signal aluop: STD_LOGIC_VECTOR(1 downto 0);
   signal branch: STD_LOGIC;
 begin
   md: maindec port map( op => op, memtoreg => memtoreg, memwrite => memwrite, branch => branch,
                        alusrc => alusrc, regdst => regdst, regwrite => regwrite, jump => jump, aluop => aluop);
-  ad: aludec port map(funct => funct, aluop => aluop, alucontrol => alucontrol);
-
+--  ad: aludec port map(funct => funct, aluop => aluop, alucontrol => alucontrol);
+    alu: ALU port map(
   pcsrc <= branch and zero;
 end;
 
